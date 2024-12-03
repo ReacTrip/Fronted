c09ed213a6593c065a71395628bf8fa6047f29cb
SrTime <dipok31@naver.com>
2024-12-03 13:20:56 +0900

    FIX: 메인페이지에 좋아요 순서대로 되지않는 오류 수정

--- src/pages/MyTripPage/MyTripPage.jsx
+++ src/pages/MyTripPage/MyTripPage.jsx
@@ -72,7 +72,7 @@ const MyTripPage = () => {
     setTrips(userTrips);
     const likedTrips = storedTrips.filter((trip) => trip.like === 1);
     setLikedTrips(likedTrips);
-  }, []);
+  });
 
   const handleAddTrip = async () => {
     if (!newTrip.title) {
@@ -188,7 +188,7 @@ const MyTripPage = () => {
             <Tab key={tab.id} label={tab.label} />
           ))}
         </Tabs>
-        
+          
         <TabContent data={tabData[tabValue].data} onCardClick={handleClick} />
       </Box>
 
